2e1f6294efcf3cc683282cffe2fb5ed92c428895
roxanamanolea <mihai.roxana90@yahoo.com>
2023-10-03 12:21:47 +0100

    Added log in button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
@@ -10,6 +10,30 @@ const App = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [playlistName, setPlaylistName] = useState("New Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [logged, setLogged] = useState(false);
+  const [userName, setUserName] = useState('');
+
+
+  useEffect(() => {
+    const authenticated = Spotify.checkAuth();
+    if (authenticated) {
+      Spotify.getUserName()
+      .then((fetchName) => {
+        setUserName(fetchName);
+        setLogged(authenticated);
+      }).catch((error) => {
+        console.error('Error fetching user name: ', error);
+      });
+    } else {
+      console.log('Login failed.');
+    }
+  }, []); 
+
+  const loginHandler = () => {
+    Spotify.getAuth();
+  };
+
+
 
   const search = useCallback((term) => {
     Spotify.search(term).then(setSearchResults);
@@ -41,28 +65,47 @@ const App = () => {
       setPlaylistName("New Playlist");
       setPlaylistTracks([]);
     });
-  }, [playlistName, playlistTracks]);
+  }, [playlistName, playlistTracks]); 
 
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <SearchBar onSearch={search} />
-        <div className="App-playlist">
-          <SearchResults searchResults={searchResults} onAdd={addTrack} />
-          <Playlist
-            playlistName={playlistName}
-            playlistTracks={playlistTracks}
-            onNameChange={updatePlaylistName}
-            onRemove={removeTrack}
-            onSave={savePlaylist}
-          />
+  if (!logged) {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className='App'>
+          <div className='loginHeader'>
+          <button className='loginButton' onClick={loginHandler}>LOG IN WITH SPOTIFY</button>
+          </div>
+          
+        </div>
+        <div className='loginFooter'>
+            <p>Thanks for visiting! | Made by Roxana</p>
+          </div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar onSearch={search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={searchResults} onAdd={addTrack} />
+            <Playlist
+              playlistName={playlistName}
+              playlistTracks={playlistTracks}
+              onNameChange={updatePlaylistName}
+              onRemove={removeTrack}
+              onSave={savePlaylist}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,29 +1,51 @@
 const clientId = 'f7e31ba8d88a4e6a815b110a831b3fd5';
 const redirectUri = 'http://localhost:3000/';
 let accessToken;
+let userId;
 
-const Spotify = {
-    getAccessToken() {
-      if (accessToken) {
-        return accessToken;
-      }
-  
-      const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-      const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-      if (accessTokenMatch && expiresInMatch) {
-        accessToken = accessTokenMatch[1];
-        const expiresIn = Number(expiresInMatch[1]);
-        window.setTimeout(() => accessToken = '', expiresIn * 1000);
-        window.history.pushState('Access Token', null, '/'); 
-        return accessToken;
+
+  const Spotify = {
+    getAuth () {
+      const tokenURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = tokenURL;
+  },
+
+  checkAuth() {
+      const authenticated = window.location.href.match(/access_token=([^&]*)/);
+      if (authenticated) {
+          accessToken = authenticated[1];
+          return true;
       } else {
-        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-        window.location = accessUrl;
+          return false;
       }
-    },
-  
+  },
+
+    getUserName() {
+      if(!accessToken) {
+          return Promise.reject(new Error('Access token is missing'));
+      }
+      const nameEndpoint = 'https://api.spotify.com/v1/me';
+      return fetch(nameEndpoint, {
+          method: 'GET',
+          headers: {
+              'Authorization': `Bearer ${accessToken}`,
+          }
+      })
+      .then((response) => {
+          if (response.status === 200) {
+              return response.json();
+          } else {
+              throw new Error('Failed to fetch user data');
+           }
+      })
+      .then((data) => {
+          const userName = data.display_name;
+          userId = data.id;
+          return userName;         
+      });
+  }, 
+
     search(term) {
-      const accessToken = Spotify.getAccessToken();
       return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
         headers: {
           Authorization: `Bearer ${accessToken}`
@@ -43,16 +65,13 @@ const Spotify = {
         }));
       });
     },
-  
+
     savePlaylist(name, trackUris) {
       if (!name || !trackUris.length) {
         return;
       }
   
-      const accessToken = Spotify.getAccessToken();
       const headers = { Authorization: `Bearer ${accessToken}` };
-      let userId;
-  
       return fetch('https://api.spotify.com/v1/me', {headers: headers}
       ).then(response => response.json()
       ).then(jsonResponse => {
@@ -71,7 +90,8 @@ const Spotify = {
           });
         });
       });
-    }
+    },
+
   };
-  
+
   export default Spotify;
